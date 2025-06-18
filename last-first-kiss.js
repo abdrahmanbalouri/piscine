fb689ceed4d3920ba10e89e9623c685d93b49451
Your Name <you@example.com>
2025-06-18 09:56:00 +0100

    hh

---
+++ last-first-kiss.js
@@ -0,0 +1,24 @@
+const first = (a)=>{
+    return a[0]
+
+
+}
+
+const last =(a)=>{
+    return a[a.length-1]
+
+
+}
+
+const  kiss = (a)=>{
+    let k = []
+    k.push(a[a.length-1])
+    k.push(a[0])
+     return k    
+
+
+
+
+}
+
+console.log(kiss([1,2,5]));
