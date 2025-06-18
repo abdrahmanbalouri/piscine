b6e9acc2a73659c4582812e399c0caf7f2bf6dc6
Your Name <you@example.com>
2025-06-18 12:30:55 +0100

    hh

--- is.js
+++ is.js
@@ -1,62 +1,61 @@
+function num(a) {
 
- function num(a){
-
-    if (typeof a =='number') {
+    if (typeof a == 'number') {
         return true
     }
     return false
 
 }
-function str(a){
-    if(typeof a =='string'){
+function str(a) {
+    if (typeof a == 'string') {
         return true
     }
     false
 }
- function nan(a){
-    if (Number.isNaN(a)){
+function nan(a) {
+    if (Number.isNaN(a)) {
         return true
     }
     return false
- }
- function bool(a){
-
-  if (typeof a =='boolean')    {
-    return true
-  }
- return false
 }
-function undef(a){
+function bool(a) {
 
-   if (typeof a =='undefined'){
-    return true
-   }
-   return false
+    if (typeof a == 'boolean') {
+        return true
+    }
+    return false
 }
-function def(a){
-  if (typeof a !=='undefined')   {
-    return true
-  }
+function undef(a) {
 
-  return false
-
-
+    if (typeof a == 'undefined') {
+        return true
+    }
+    return false
 }
-
-function arr(a){
-    if (Array.isArray(a)){
+function def(a) {
+    if (typeof a !== 'undefined') {
         return true
     }
 
-  return false
+    return false
+
+
 }
 
-function obj(a){
+function arr(a) {
+    if (Array.isArray(a)) {
+        return true
+    }
 
-    if (a instanceof Map)return false
-        if (a instanceof Set)return false
+    return false
+}
+
+function obj(a) {
+
+    if (a instanceof Map) return false
+    if (a instanceof Set) return false
     if (arr(a)) return false
-    if (typeof a == 'object'&& a!==null ){
+    if (typeof a == 'object' && a !== null) {
         return true
     }
 
@@ -66,46 +65,64 @@ function obj(a){
 }
 
 
-function fun(a){
+function fun(a) {
 
-    if (typeof a =='function'){
+    if (typeof a == 'function') {
         return true
     }
-   return false
+    return false
 }
-function truthy(a){
-    if (a!==false && a!==0&& a!==-0&& a!==0n&&a!==""&&a!==null&&a!==undefined&&a!==NaN){
+function truthy(a) {
+    if (a !== false && a !== 0 && a !== -0 && a !== 0n && a !== "" && a !== null && a !== undefined && a !== NaN) {
         return true
     }
 
-   return false
+    return false
 
 }
 
-function falsy(a){
+function falsy(a) {
+    if (typeof a =='object'){
+  for (let i= 0;i<a.length;i++){
+    
+         if (a[i]== false|| a[i]== 0 || a[i]== -0 || a[i]== 0n || a[i]== '' || a[i]== null || a[i]== undefined || a[i]== NaN) {
+        return true
+    }
+
+
+  }
+   
+            
+   
+            
+         return false
+
+    }
+   
+
     
 
-
-if (a==false || a==0|| a==-0|| a==0n||a==""||a==null||a==undefined||a==NaN){
+    if (a == false || a == 0 || a == -0 || a == 0n || a == '' || a == null || a == undefined || a == NaN) {
         return true
     }
+
     return false
 
 
 }
 
 is.num = num
-is.nan=nan
-is.str =str
-is.bool=bool
-is.undef =undef
-is.def =def
-is.arr=arr
-is.obj=obj
-is.fun=fun
-is.truthy=truthy
-is.falsy=falsy
+is.nan = nan
+is.str = str
+is.bool = bool
+is.undef = undef
+is.def = def
+is.arr = arr
+is.obj = obj
+is.fun = fun
+is.truthy = truthy
+is.falsy = falsy
 
 
 
-console.log(is.nan(NaN));
+console.log(is.falsy([0, NaN, '', undefined, null, void 0]));
