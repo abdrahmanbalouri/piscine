8c01a581df9d7116331521cb1f9ead71bbc7e2f0
Your Name <you@example.com>
2025-06-18 10:49:52 +0100

    hh

---
+++ min-max.js
@@ -0,0 +1,18 @@
+const max = (a,b)=>{
+
+    if (a>b){
+        return a
+    }
+
+  return b
+}
+const min = (a,b)=>{
+
+    if (a<b){
+        return a
+    }
+
+  return b
+}
+
+console.log(min(4,9));
