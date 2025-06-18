5bc763b8196dda355b507f37b05ed4e99bb8497b
Your Name <you@example.com>
2025-06-18 09:33:39 +0100

    hh

--- mutability.js
+++ mutability.js
@@ -4,9 +4,9 @@ const person = {
   country: 'US',
 }
 
- clone1 = person 
- clone2 =person
- samePerson =person
+ const clone1 = person 
+ const clone2 =person
+ const samePerson =person
  person.age+=1
  person.country ='FR'
  console.log(clone1,clone2,samePerson);
