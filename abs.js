c81578d92cc5ef062e386e7c2f395eef51e1b46d
Your Name <you@example.com>
2025-06-18 10:43:43 +0100

    hh

---
+++ abs.js
@@ -0,0 +1,20 @@
+ const isPositive = (a)=>{
+
+    if (a>=0){
+        return true
+    }
+
+    return false
+ }
+
+ const abs =(a)=>{
+    if (a>=0){
+        return a
+    }
+  return -a
+
+
+ }
+
+ console.log(abs(-5));
+ 
