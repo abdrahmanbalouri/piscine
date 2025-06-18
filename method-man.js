e9bc9a2d36668abe44bf05f6789c694dcd703f5b
Your Name <you@example.com>
2025-06-18 10:37:03 +0100

    hh

---
+++ method-man.js
@@ -0,0 +1,35 @@
+const words = (a) => {
+
+
+ return   a.split(" ")
+}
+
+const sentence = (a)=>{
+
+    return a.join(" ")
+
+
+
+}
+
+const yell = (a)=>{
+
+    return a.toUpperCase()
+
+
+}
+const whisper =(a)=>{
+    return a.toLowerCase()
+
+
+}
+const capitalize =(a)=>{
+
+    return a[0].toUpperCase()+a.slice(1,).toLowerCase()
+
+
+
+}
+
+
+
