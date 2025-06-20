c5ac6f019b3897bf07cc66f58fda70d879d16dc6
Your Name <you@example.com>
2025-06-20 15:38:56 +0100

    hh

---
+++ unbreakable.js
@@ -0,0 +1,48 @@
+const split = (a,target)=>{
+
+
+    let k =[]
+    let v=''
+
+    for (let i=0;i<a.length;i++){
+
+        if (a[i]===' '&& v!==''){
+
+            k.push(v)
+            v=''
+
+        }else{
+
+            v+=a[i]
+        }
+
+
+    }
+  if (v!==''){
+    console.log(v);
+    
+    k.push(v)
+  }
+ return k
+}
+
+const join =(a,target)=>{
+
+    let k =''
+
+    for(let i=0;i<a.length;i++){
+
+        k+= a[i]
+        if (i!==a.length-1){
+                    k+= target
+
+        }
+
+
+    }
+    
+  return k
+
+
+}
+ 
