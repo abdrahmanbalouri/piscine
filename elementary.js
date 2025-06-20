c0032633d1f0a13959ed0073df4f2970f4751081
Your Name <you@example.com>
2025-06-20 09:32:56 +0100

    gg

---
+++ elementary.js
@@ -0,0 +1,92 @@
+const multiply= (a,b)=>{
+
+    let k = false
+
+    if (a<0&&b<0){
+        a=-a
+        b=-b
+    }
+    if (a<0){
+        a=-a    
+        k= true    
+    }
+    if (b<0){
+        b=-b
+        k=true
+    }
+    let c =a
+
+    for (let i =1;i<b;i++){
+        c+=a
+
+    }
+
+    if  (k){
+        return -c
+    }
+    return c
+
+
+}
+
+const divide = (a,b)=>{
+ let k = false
+
+    if (a<0&&b<0){
+        a=-a
+        b=-b
+    }
+    if (a<0){
+        a=-a    
+        k= true    
+    }
+    if (b<0){
+        b=-b
+        k=true
+    }
+    let c =a
+    let i =0
+
+    while (c-b>=0){
+        c-=b
+        i++
+    }
+
+    if (k){
+        return -i
+    }
+
+    return i
+
+
+}
+
+
+const modulo =(a,b)=>{
+
+
+let k = false
+
+  
+    if (a<0){
+        a=-a    
+    }
+    if (b<0){
+        b=-b
+        k =true
+    }
+    let c =a
+
+    while (c-b>=0){
+        c-=b
+    }
+
+    if (k){
+        return -c
+    }
+
+    return c
+
+}
+
+
