363ff5173fd89030a07ae4081824c2c29dea2a21
Your Name <you@example.com>
2025-06-20 09:58:57 +0100

    gg

--- index-of.js
+++ index-of.js
@@ -15,8 +15,8 @@ const indexOf =(a,b,c)=>{
 
 
 }
-const lastIndexOfd = (arr ,target)=>{
-    for(let i =arr.length-1;i>=0;i++){
+const lastIndexOf = (arr ,target)=>{
+    for(let i =arr.length-1;i>=0;i--){
 
         if (arr[i]===target){
             return i
@@ -38,6 +38,9 @@ const  includes = (arr,target)=>{
 
     return false
 }
+
+console.log(lastIndexOf([1, 2, 3, 4, 5, 4, 3, 2, 1], 2));
+
 
 
 
