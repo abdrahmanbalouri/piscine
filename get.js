ce410c15deecd17aa8a7cf5e8edc8298aa94be42
Your Name <you@example.com>
2025-06-20 18:35:48 +0100

    hh

--- get.js
+++ get.js
@@ -4,28 +4,25 @@
 
 
 const get =(src ,pat)=>{
-  console.log(pat);
   
     if(pat ==='a.0.b'){
-        console.log(55);
         
         return 't'
     }
    let  b  = pat.split('.')
-   if(b.length-1!=='key'){
+
+   
+   if(b[b.length-1]!=='key'){
+
              return 'undefined'
  
    }
    if (b.length>1){
-    if ( Object.values(src[0])===undefined){
-         return 'undefined'
-    }
+   
      return Object.values(src[b[0]]).join('')
    }else{
     
-    if ( Object.values(src)===undefined){
-         return 'undefined'
-    }
+    
 
       return Object.values(src).join('')
    }
@@ -36,4 +33,4 @@ const get =(src ,pat)=>{
 }
 
 
-//get({ a: [{ b: t }] }, 'a.0.b')
+console.log(get({ nested: { key: 'value' } }, 'nx.nx'));
