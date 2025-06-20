71e70eb862443773953bd22cb4c4b58608400c65
Your Name <you@example.com>
2025-06-20 09:53:03 +0100

    gg

---
+++ index-of.js
@@ -0,0 +1,45 @@
+const indexOf =(a,b,c)=>{
+    if (c==undefined){
+        c = 0
+    }
+
+    for(let i=c;i<a.length;i++){
+
+        if (a[i]===b){
+            return i
+
+
+        }
+    }
+    return -1
+
+
+}
+const lastIndexOfd = (arr ,target)=>{
+    for(let i =arr.length-1;i>=0;i++){
+
+        if (arr[i]===target){
+            return i
+        }
+    }
+    return -1
+
+
+}
+
+
+const  includes = (arr,target)=>{
+
+    for(let i=0;i<arr.length;i++){
+        if (arr[i]===target){
+            return true
+        }
+    }
+
+    return false
+}
+
+[1,2,3,4].indexOf(2)
+
+
+ 
