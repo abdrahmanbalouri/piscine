88b4316c2757968870ce3c3bd3c3bc336dbf9807
Your Name <you@example.com>
2025-06-21 10:14:29 +0100

    gg

--- pyramid.js
+++ pyramid.js
@@ -1,15 +1,20 @@
 const  pyramid=(str,num)=>{
     let k =[]
-
-    for (let i =0;i<num;i++){
-        const bb = ' '.repeat(num-1-i)
+  
+      for (let i =0;i<num;i++){
+        const bb = ' '.repeat((num-1-i)*str.length)
+        
 
   const  kk = str.repeat(i*2+1) 
 
     k.push(bb+kk)
 
     }
+   
+   
+  
 
   return k.join('\n')
 }
 
+console.log(pyramid('{}', 12));
