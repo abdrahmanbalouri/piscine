ea5470737e54f1030d3cead114d01b86ffca563f
Your Name <you@example.com>
2025-06-21 11:38:59 +0100

    gg

--- block-chain.js
+++ block-chain.js
@@ -1,42 +1,23 @@
+const blockChain =(data,prev ={ index: 0, hash: '0' })=>{
 
-const hashCode = str =>
-  (
-    [...str].reduce((h, c) => (h = (h << 5) - h + c.charCodeAt(0)) & h, 0) >>> 0
-  ).toString(36)
-
-
-const data = {
-  a: 1
-}
-const prev = {
-  index: 0,
-  hash: '0'
-}
-
-const blockChain = (data, prev) => {
-  const s = {}
+  return {
+    data :data,
+    prev: prev,
+    index: prev.index+1,
+    hash : hashCode(prev.index+1 + prev.hash+JSON.stringify(data) ),
 
 
-  s['data'] = data
-  s['prev'] = prev
-  s['hash'] = hashCode(data)
+     chain : function(newdata){
+      return  blockChain(newdata ,this)
+     }
 
 
-  return s
+
+
+
+  }
 
 
 }
-const kk = blockChain(data, prev)
-
-console.log(kk);
 
 
-const nums = [Math.PI, -Math.PI, Math.E, -Math.E, 0]
-
-t(({ code }) => !/String|['"`]|toFixed|slice/.test(code))
-t(({ code }) => !code.includes('~'))
-t(({ code }) => !code.includes('%'))
-t(({ code }) => !code.includes('>>'))
-t(({ code }) => !/[^|]\|[^|]/.test(code))
-t(({ code }) => !/[^&]&[^&]/.test(code))
-t(({ code }) => !code.includes('parseInt'))
