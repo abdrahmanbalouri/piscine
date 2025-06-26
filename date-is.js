408f628d0644fa0d46863bb7d3430f1ad15ccbe6
Your Name <you@example.com>
2025-06-26 09:36:55 +0100

    j

--- date-is.js
+++ date-is.js
@@ -10,4 +10,56 @@ const isValid =(date)=>{
   return true
 }
 
-console.log(isValid('hgvhu'));
+const isAfter =(date ,date2)=>{
+    if(isValid(date) == false) return false
+
+    if(isValid(date2) == false) return false
+
+    if(date.getTime()>date2.getTime()){
+             return true
+    }
+
+
+ return false
+
+}
+const isBefore =(date,date2)=>{
+
+   if(isValid(date) == false) return false
+
+    if(isValid(date2) == false) return false
+
+    if(date.getTime()<date2.getTime()){
+             return true
+    }
+
+
+ return false
+
+
+
+}
+const isFuture =(date)=>{
+ if(isValid(date) == false) return false
+
+
+    if(date.getTime()>  Date.now()){
+             return true
+    }
+
+
+ return false
+
+}
+const isPast =(date)=>{
+ if(isValid(date) == false) return false
+
+
+    if(date.getTime()<  Date.now()){
+             return true
+    }
+
+
+ return false
+
+}
