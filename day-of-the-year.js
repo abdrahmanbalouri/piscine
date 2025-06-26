e59b92e8173abad0c0010a79a80998c3b05e1bca
Your Name <you@example.com>
2025-06-26 17:45:41 +0100

    l

---
+++ day-of-the-year.js
@@ -0,0 +1,22 @@
+const dayOfTheYear=(date)=>{
+
+    let datef= new Date('0001-01-01')
+    let c = date.getFullYear()
+
+    datef.setFullYear(c-1)
+    console.log(datef);
+    
+
+    
+
+    let k = Math.floor(((date-datef)/(1000*60*60*24)))+1
+
+    return k
+       
+
+
+
+
+}
+
+console.log(  dayOfTheYear(new Date('2048-11-08')) );
