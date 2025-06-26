8e53c2cc71b6c5061368b3081abd34d9c14029fe
Your Name <you@example.com>
2025-06-26 18:45:48 +0100

    zero the goat

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -4,26 +4,15 @@ const dayOfTheYear=(date)=>{
     let c = date.getFullYear()
 
     datef.setFullYear(c-1)
-    let k = Math.floor(((date-datef)/(1000*60*60*24)))+1
 
-     console.log(44);
+    
+    let k = Math.floor(((date-datef)/(1000*60*60*24)))+1
      
     if (k==367){
         return 1
     }
-   
-
-
-
-
-
-
     return k
        
-
-
-
-
 }
 
-console.log(dayOfTheYear(new Date('1664-08-09')));
+// console.log( dayOfTheYear(new Date('1664-08-09')))
