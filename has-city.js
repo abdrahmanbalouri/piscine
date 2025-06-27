920f309fec6f2446c5cb95a8b30fe3ddfa332333
Your Name <you@example.com>
2025-06-27 16:54:30 +0100

    hh

---
+++ has-city.js
@@ -0,0 +1,20 @@
+const hasCity = (country, arr) => {
+    return function (city) {
+
+        if (arr.includes(city)) {
+
+
+            return `${city} is a city from ${country}`
+
+        } else {
+
+            return `${city} is not a city from ${country}`
+
+
+        }
+
+
+    }
+
+
+}
