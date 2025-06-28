0b3ef357bce2a6583767b372f18ea15a1e242edb
Your Name <you@example.com>
2025-06-28 17:34:11 +0100

    hh

---
+++ harder-bigger-bolder-stronger.js
@@ -0,0 +1,42 @@
+export  function  generateLetters(){
+
+    let zize = 11
+  //  const body = document.querySelector('body')
+     let b = 'AZERTYUIOPMLKJHGFDSQWXCVBN'    
+
+    for(let i =0;i<=120;i++){
+
+ 
+     if (i<120 /3){
+        const kalma = document.createElement('div')
+      kalma.textContent=  b[Math.floor(Math.random()*b.length) ]
+      kalma.style.fontSize = `${zize}px`
+      kalma.style.fontWeight = '300px'
+      zize++
+           
+        document.body.append(kalma)
+     }else if (i< (120/3)*2){
+
+        const kalma = document.createElement('div')
+      kalma.textContent=  b[Math.floor(Math.random()*b.length) ]
+      kalma.style.fontSize = `${zize}px`
+      kalma.style.fontWeight = '400px'
+      zize++
+           
+        document.body.append(kalma)
+     }else{
+          const kalma = document.createElement('div')
+      kalma.textContent=  b[Math.floor(Math.random()*b.length) ]
+      kalma.style.fontSize = `${zize}px`
+      kalma.style.fontWeight = '600px'
+      zize++
+      console.log(zize);
+      
+           
+        document.body.append(kalma)
+
+     }
+    
+    }
+      
+}
