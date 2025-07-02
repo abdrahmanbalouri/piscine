6037d6906e5934e7cae1f885a314d4eadf0ec92d
Your Name <you@example.com>
2025-07-02 16:08:00 +0100

    hh

--- fusion.js
+++ fusion.js
@@ -4,15 +4,32 @@ const fusion = (...objects) => {
   for (let i = 0; i < objects.length; i++) {
     for (const v in objects[i]) {
       if (k.has(v)) {
-        
-        k.get(v).push(...objects[i][v]);
+         if(Array.isArray(objects[i][v])){
+                    k.get(v).push(...objects[i][v]);
+
+         }else{
+         let b =k.get(v);
+                     k.set(v,b+objects[i][v])
+
+
+         }
       } else {
-        k.set(v, [...objects[i][v]]);
+        if(Array.isArray(objects[i][v])){
+               k.set(v, [...objects[i][v]]);
+        }else{
+
+            k.set(v,objects[i][v])
+        }
+     
       }
+
+
+    console.log(objects[i][v]);
+    
     }
   }
 
   return Object.fromEntries(k);
 };
 
-console.log(fusion({ arr: [], arr1: [5] },{ arr: [10, 3], arr1: [15, 3], arr2: ["7", "1"] }));
+console.log(fusion({ a: 1, b: { c: "Salem" } }, { a: 10, x: [], b: { c: "alem" } }));
