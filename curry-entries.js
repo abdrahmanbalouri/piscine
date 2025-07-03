0caa6ab6b5351ed2d2b126aa619e2441962a2976
Your Name <you@example.com>
2025-07-03 17:58:17 +0100

    j

--- curry-entries.js
+++ curry-entries.js
@@ -1,10 +1,4 @@
-const personnel = {
-  lukeSkywalker: { id: 5,  pilotingScore: 98, shootingScore: 56, isForceUser: true  },
-  sabineWren:    { id: 82, pilotingScore: 73, shootingScore: 99, isForceUser: false },
-  zebOrellios:   { id: 22, pilotingScore: 20, shootingScore: 59, isForceUser: false },
-  ezraBridger:   { id: 15, pilotingScore: 43, shootingScore: 67, isForceUser: true  },
-  calebDume:     { id: 11, pilotingScore: 71, shootingScore: 85, isForceUser: true  },
-}
+
 const defaultCurry =(object)=>{
     return function(obje){
     return{...object,...obje}
@@ -89,4 +83,3 @@ const mapAverage =(objet)=>{
 
 
 }
-console.log((mapAverage(personnel)));
