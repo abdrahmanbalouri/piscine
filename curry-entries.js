73f3df8f10c9da203cb290a4b69252ecb4757db7
Your Name <you@example.com>
2025-07-03 17:51:46 +0100

    j

--- curry-entries.js
+++ curry-entries.js
@@ -1,4 +1,10 @@
-
+const personnel = {
+  lukeSkywalker: { id: 5,  pilotingScore: 98, shootingScore: 56, isForceUser: true  },
+  sabineWren:    { id: 82, pilotingScore: 73, shootingScore: 99, isForceUser: false },
+  zebOrellios:   { id: 22, pilotingScore: 20, shootingScore: 59, isForceUser: false },
+  ezraBridger:   { id: 15, pilotingScore: 43, shootingScore: 67, isForceUser: true  },
+  calebDume:     { id: 11, pilotingScore: 71, shootingScore: 85, isForceUser: true  },
+}
 const defaultCurry =(object)=>{
     return function(obje){
     return{...object,...obje}
@@ -39,7 +45,7 @@ const filterCurry =(fn)=>{
 
 
 }
-const reduceScore =(objet )=>{
+const reduceScore =(objet ,acc)=>{
   return reduceCurry((acc ,[_,v])=>{
 
  if( v.isForceUser){
@@ -54,7 +60,7 @@ const reduceScore =(objet )=>{
  }
 
 
-  })(objet,0)
+  })(objet,acc=0)
 
 
 
@@ -83,4 +89,4 @@ const mapAverage =(objet)=>{
 
 
 }
-console.log(mapAverage(personnel));
+console.log((reduceScore(personnel)));
