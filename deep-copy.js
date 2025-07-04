aa65b790b593f3fb4151051520f312d6b13ae635
Your Name <you@example.com>
2025-07-04 11:28:54 +0100

    jj

---
+++ deep-copy.js
@@ -0,0 +1,21 @@
+const deepCopy =(objet)=>{
+   if(typeof objet !=='object'){
+
+    return objet
+   }
+
+
+   let cop = Array.isArray(objet)?[]:{}
+
+   for(key in objet){
+   
+    cop [key] = deepCopy(objet[key])
+   }
+   return cop
+}
+
+
+
+
+// console.log(deepCopy({k:4,m:{n:7}}));
+
